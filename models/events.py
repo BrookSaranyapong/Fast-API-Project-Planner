594f31e094e05d50e4dac62ee5971dc2ccf0c473
Brooksaranyapong <brook.saranyapong@gmail.com>
2023-12-02 15:24:58 +0700

    add connection with SQLModel

--- models/events.py
+++ models/events.py
@@ -1,9 +1,28 @@
-from pydantic import BaseModel
+from sqlmodel import JSON,SQLModel,Field, Column
 from typing import List
 
 
-class Event(BaseModel):
-    id:int
+class Event(SQLModel,table=True):
+    id:int = Field(default=None,primary_key=True)
+    title: str
+    image: str
+    description: str
+    tags: List[str] = Field(sa_column=Column(JSON))
+    location: str
+
+    class Config:
+        arbitrary_types_allow = True
+        schema_extra = { 
+            "example" : {
+                "title":"FastAPI Events list" ,
+                "image" : "https://my-image.com/img.png",
+                "description": "Welcome to event project",
+                "tags": ["FastAPI","Python","Devforcat"],
+                "location":"Google Meet"
+            }
+        }
+
+class EventUpdate(SQLModel):
     title: str
     image: str
     description: str
@@ -11,9 +30,8 @@ class Event(BaseModel):
     location: str
 
     class Config:
-        json_schema_extra = { 
+        schema_extra = { 
             "example" : {
-                "id" : 1,
                 "title":"FastAPI Events list" ,
                 "image" : "https://my-image.com/img.png",
                 "description": "Welcome to event project",
